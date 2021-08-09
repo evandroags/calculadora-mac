02ec615b149b4ac2d279e834ca4980d51cc4af37
Evandro Augusto Guaranha Da Silva <evandroags@hotmail.com>
2021-08-09 01:01:58 -0300

    final version calculate - logic memory

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -3,11 +3,17 @@ class Memory {
 
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
-  String operation = '';
+  String _operation = '';
   String _value = '0';
   bool _wipeValue = false;
+  String _lastCommand = '';
 
   void applyCommand(String command) {
+    if (isReplacingOperation(command)) {
+      _operation = command;
+      return;
+    }
+
     if (command == 'AC') {
       _allClear();
     } else if (operations.contains(command)) {
@@ -15,9 +21,35 @@ class Memory {
     } else {
       _addDigit(command);
     }
+
+    _lastCommand = command;
+  }
+
+  isReplacingOperation(String command) {
+    return operations.contains(_lastCommand) &&
+        operations.contains(command) &&
+        _lastCommand != '=' &&
+        command != '=';
   }
 
   _setOperation(String newOperation) {
+    bool isEqualSign = newOperation == '=';
+
+    if (_bufferIndex == 0) {
+      if (!isEqualSign) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0].toString();
+      _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
+
+      _operation = isEqualSign ? '' : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
+    }
     _wipeValue = true;
   }
 
@@ -35,11 +67,31 @@ class Memory {
     _wipeValue = false;
 
     _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
-    print(_buffer[_bufferIndex]);
   }
 
   _allClear() {
     _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _bufferIndex = 0;
+    _operation = '';
+    _wipeValue = false;
+  }
+
+  _calculate() {
+    switch (_operation) {
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case 'X':
+        return _buffer[0] * _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      default:
+        return _buffer[0];
+    }
   }
 
   String get value {
